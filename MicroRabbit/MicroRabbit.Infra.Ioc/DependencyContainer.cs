1330e38e9e3d66d8d83ea98a0cd92673a832c288
SandhuManpreet <sandhu.manpreet366@gmail.com>
2020-06-28 00:54:56 -0600

    Registered IEventBus and RabbitMqBus in Dependency Container.

---
+++ MicroRabbit/MicroRabbit.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMq.Infra.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Infra.Ioc
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMqBus>();
+        }
+    }
+}
